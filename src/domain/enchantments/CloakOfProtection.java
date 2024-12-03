11f88fb01b4ff735893a017c8fb85fd04b93e077
brahim Cebecioğlu <icebecioglu21@ku.edu.tr>
2024-12-03 12:21:52 +0300

    write Cloak Of Protection

--- src/domain/enchantments/CloakOfProtection.java
+++ src/domain/enchantments/CloakOfProtection.java
@@ -1,5 +1,24 @@
 package domain.enchantments;
 
+/**
+ * This enchantment provides invisibility for given duration time against monsters
+ */
 public class CloakOfProtection{
+    private int effectDuration;
 
+    /**
+     * gets the duration time of cloak of protection enchantment
+     * @return
+     */
+    public int getEffectDuration() {
+        return effectDuration;
+    }
+
+    /**
+     * sets the duration time of cloak of protection enchantment
+     * @param effectDuration
+     */
+    public void setEffectDuration(int effectDuration) {
+        this.effectDuration = effectDuration;
+    }
 }
