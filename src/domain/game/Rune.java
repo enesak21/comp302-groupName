23931331c5ce216954c436d9aa65eaf82987ff5f
TavarishYosef <yusufkaratas22@ku.edu.tr>
2024-12-22 20:10:05 +0300

    Add comments to clarify image usage and setStoredStructure method in Rune class

--- src/domain/game/Rune.java
+++ src/domain/game/Rune.java
@@ -9,7 +9,7 @@ import domain.structures.Structure;
 
 public class Rune {
     private Structure storedStructure;
-    private BufferedImage image;
+    private BufferedImage image; // Image of the rune to be displayed after Rune is found
 
     public Rune(Structure structure) {
         setStoredStructure(structure);
@@ -28,6 +28,12 @@ public class Rune {
         return storedStructure;
     }
 
+    /**
+     * Sets the stored structure for this rune. If the rune was previously stored in another structure,
+     * it removes the rune from that structure before setting the new structure.
+     *
+     * @param storedStructure the new structure to store the rune in
+     */
     public void setStoredStructure(Structure storedStructure) {
         Structure oldStructure = this.storedStructure;
         if (oldStructure != null) { // If the rune was in a structure, remove the rune from it
