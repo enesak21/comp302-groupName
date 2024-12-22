1d86ae49c95add6f0e23e82b761b14c10271429b
TavarishYosef <yusufkaratas22@ku.edu.tr>
2024-12-22 17:07:34 +0300

    Add getStructures method to Grid

--- src/domain/game/Grid.java
+++ src/domain/game/Grid.java
@@ -8,6 +8,7 @@ import domain.structures.Structure;
 
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.awt.image.BufferedImage;
 
 public class Grid {
@@ -92,5 +93,18 @@ public class Grid {
 
     public void setTileSize(int tileSize) {
         this.tileSize = tileSize;
+    }
+
+    public ArrayList<Structure> getStructures() {
+        ArrayList<Structure> structures = new ArrayList<>();
+        for (int column = 0; column < columns; column++) {
+            for (int row = 0; row < rows; row++) {
+                Tile tile = tiles[column][row];
+                if (tile != null && tile.getStructure() != null) {
+                    structures.add(tile.getStructure());
+                }
+            }
+        }
+        return structures;
     }
 }
--- src/domain/game/Rune.java
+++ src/domain/game/Rune.java
@@ -4,6 +4,7 @@ import domain.structures.Structure;
 
 public class Rune {
     private Structure storedStructure;
+    
     public Rune(Structure structure) {
         this.storedStructure = structure;
     }
