259a4a9a58d722eab76e6be1550eedf62632c272
brahim Cebecioğlu <icebecioglu21@ku.edu.tr>
2024-12-22 17:56:07 +0300

    switchRune method implemented

--- src/domain/entity/monsters/WizardMonster.java
+++ src/domain/entity/monsters/WizardMonster.java
@@ -2,17 +2,28 @@ package domain.entity.monsters;
 
 import domain.entity.Entity;
 import domain.entity.playerObjects.Player;
+import domain.game.Game;
+import domain.game.Tile;
+import domain.structures.Structure;
+
+import java.util.List;
+import java.util.Random;
 
 public class WizardMonster extends BaseMonster{
-    private float teleportFrequency = 5; //It teleports the rune in every 5 sec.
+    private final long TELEPORT_FREQUENCY = 5000; //It teleports the rune in every 5 sec.
+    private long lastTeleportTime;
+    private Random random;
+    private Game game;
 
-    public WizardMonster(int gridX, int gridY, int tileSize) {
+    public WizardMonster(int gridX, int gridY, int tileSize, Game game) {
         super(gridX, gridY, tileSize);
+        this.lastTeleportTime= System.currentTimeMillis();
+        this.random = new Random();
+        this.game=game;
     }
 
     @Override
     public void update() {
-
     }
 
     @Override
@@ -23,7 +34,20 @@ public class WizardMonster extends BaseMonster{
 
     @Override
     public void attack(Player player) {
-        //a.k.a switchRune()
+        long currentTime = System.currentTimeMillis();
+        if ((currentTime - lastTeleportTime) > TELEPORT_FREQUENCY) {
+            switchRune();
+            lastTeleportTime = currentTime;
+        }
+    }
+
+    private void switchRune() {
+        List<Structure> structures = game.getGrid().getStructures();
+        if (!structures.isEmpty()) {
+            int selectedIndex = random.nextInt(structures.size());
+            Structure selectedStructure = structures.get(selectedIndex);
+            game.getRune().setStoredStructure(selectedStructure);
+        }
     }
 
     @Override
