26052cc1039b7490913603536d4d229238d2c096
cemalnisan <cnisan21@ku.edu.tr>
2024-12-24 23:31:06 +0300

    MIN_NUM_STRUCTURE = 0 for Halls

--- src/domain/game/HallOfAir.java
+++ src/domain/game/HallOfAir.java
@@ -1,7 +1,9 @@
 package domain.game;
 
 public class HallOfAir extends Hall {
-    private static final int MIN_NUM_STRUCTURE = 9;
+    //private static final int MIN_NUM_STRUCTURE = 9; //For the test, we set the minimum number of structures to 0
+    private static final int MIN_NUM_STRUCTURE = 0;
+
 
     public HallOfAir() {
         super("Hall of Air", MIN_NUM_STRUCTURE);
