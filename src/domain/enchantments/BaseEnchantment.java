445251d5e6cc2dcdf0f9b0fa9aecdd1a1dfd6cc6
Caner Kösem <ckosem21@ku.edu.tr>
2025-01-05 12:20:43 +0300

    enchantment1 new classes created

---
+++ src/domain/enchantments/BaseEnchantment.java
@@ -0,0 +1,4 @@
+package domain.enchantments;
+
+public class BaseEnchantment {
+}
---
+++ src/domain/enchantments/ExtraTimeFactory.java
@@ -0,0 +1,6 @@
+package domain.enchantments;
+
+
+//first will be used for trial
+public class ExtraTimeFactory {
+}
