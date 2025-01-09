c9daf7469a89cf73043fa22db9a92a6a8334300a
TavarishYosef <yusufkaratas22@ku.edu.tr>
2025-01-09 20:29:33 +0300

    Add testTileGeneratorGridDimensions and testTileGeneratorUniqueness tests

--- src/test/GridTest.java
+++ src/test/GridTest.java
@@ -2,12 +2,14 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
 import org.junit.BeforeClass;
 
 import domain.game.Grid;
+import domain.game.Tile;
 
 public class GridTest {
 
@@ -24,7 +26,7 @@ public class GridTest {
     }
 
     @Test
-    public void testTileGenerator() {
+    public void testTileGeneratorTileNotNull() {
         grid.tileGenerator();
         for (int column = 0; column < grid.getColumns(); column++) {
             for (int row = 0; row < grid.getRows(); row++) {
@@ -32,4 +34,21 @@ public class GridTest {
             }
         }
     }
+
+    @Test
+    public void testTileGeneratorGridDimensions() {
+        grid.tileGenerator();
+        assertEquals("Grid should have correct number of rows", 16, grid.getRows());
+        assertEquals("Grid should have correct number of columns", 16, grid.getColumns());
+    }
+
+    @Test
+    public void testTileGeneratorUniqueness() {
+        grid.tileGenerator();
+        Tile tile1 = grid.getTileAt(0, 0);
+        Tile tile2 = grid.getTileAt(1, 1);
+        assertNotEquals("Tiles at different positions should not be identical", tile1, tile2);
+    }
+
+
 }
