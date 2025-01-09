15f34c63dc4fff9bb74f1ed3254795c8f5120e75
muhbabelli <mbabelli22@ku.edu.tr>
2025-01-09 18:58:15 +0300

    fixed game winning screen positions

--- src/domain/handlers/GameWinningHandler.java
+++ src/domain/handlers/GameWinningHandler.java
@@ -28,16 +28,16 @@ public class GameWinningHandler {
     private void drawWinningScreen(Graphics2D g2) {
         // Draw a semi-transparent dark overlay
         g2.setColor(new Color(0, 0, 0, 150)); // Semi-transparent black
-        g2.fillRect(0, 0, screenWidth, screenHeight);
+        g2.fillRect(0, 0, playModePanel.getWidth(), playModePanel.getHeight());
 
-        // Draw the game over message
-        g2.setFont(font.deriveFont(40f));
+        // Draw the game winning message
+        g2.setFont(font.deriveFont(30f));
         g2.setColor(Color.GREEN); // Change the color to green
 
         FontMetrics fm = g2.getFontMetrics();
         String congratsText = "Congrats, You Won!";
-        int congratsX = (screenWidth - fm.stringWidth(congratsText)) / 2;
-        int congratsY = (screenHeight - fm.getHeight()) / 2 + fm.getAscent();
+        int congratsX = (playModePanel.getWidth() - fm.stringWidth(congratsText)) / 2;
+        int congratsY = (playModePanel.getHeight() - fm.getHeight()) / 2 + fm.getAscent();
 
         g2.drawString(congratsText, congratsX, congratsY);
 
@@ -46,7 +46,7 @@ public class GameWinningHandler {
         int buttonHeight = 50;
 
         // Calculate button positions
-        int startX = (screenWidth - buttonWidth) / 2;
+        int startX = (playModePanel.getWidth() - buttonWidth) / 2;
         int buttonY = congratsY + fm.getHeight() + 40; // Position below the text
 
         // Draw "Try Again" button
