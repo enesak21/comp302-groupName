ebc90b388b9a761dd9a33e6ed9d7b283b73658e2
enesak21 <115219739+enesak21@users.noreply.github.com>
2025-01-22 00:59:01 +0300

    bug fix

--- src/domain/UI/panels/PlayModePanel.java
+++ src/domain/UI/panels/PlayModePanel.java
@@ -191,6 +191,7 @@ public class PlayModePanel extends JPanel implements Runnable {
     }
 
     public void initializeGameComponents(GameState loadedGame) {
+        hallNum = loadedGame.getHallNum();
         this.setState("Default");
         isPaused = false;
 
